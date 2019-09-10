61fb4bca82c14ac03259c2e34a6b7c462ed6a1a6
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-10 15:09:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Unlock Protocol Wordpress plugin
+
+This Wordpress plugin lets you put content of your pages and posts behind a lock.
+Start by [deploying your first lock](https://unlock-protocol.com/blog/create-first-lock/).
+
+Please contact us with any question or suggestion!
---
+++ unlock-blocks.js
@@ -0,0 +1,110 @@
+const { registerBlockType, getBlockTypes } = window.wp.blocks
+
+// Block for content that is only available to owners of keys!
+registerBlockType("unlock/unlocked-box", {
+  title: "Unlocked Box - Visible to members only",
+  icon: "lock",
+  category: "common",
+  attributes: {
+    content: { type: "string" }
+  },
+
+  edit: props => {
+    const onChangeContent = value => {
+      props.setAttributes({ content: value })
+    }
+    return wp.element.createElement(wp.editor.RichText, {
+      placeholder:
+        "Add your content which will only be visible by key holders!",
+      value: props.attributes.content,
+      onChange: onChangeContent,
+      className: "unlock__unlocked-box"
+    })
+  },
+
+  save: props => {
+    return wp.element.createElement(wp.editor.RichText.Content, {
+      tagName: "p",
+      className: "unlock-protocol__unlocked",
+      value: props.attributes.content
+    })
+  }
+})
+
+// Block for content that is only available for people with no key
+registerBlockType("unlock/locked-box", {
+  title: "Locked Box - Visible to non members only",
+  icon: "lock",
+  category: "common",
+  attributes: {
+    content: { type: "string" }
+  },
+
+  edit: props => {
+    const onChangeContent = value => {
+      props.setAttributes({ content: value })
+    }
+    return wp.element.createElement(wp.editor.RichText, {
+      placeholder:
+        "Add your content which will only be visible by users who do not have a key yet!",
+      value: props.attributes.content,
+      onChange: onChangeContent,
+      className: "lock__unlocked-box"
+    })
+  },
+
+  save: props => {
+    return wp.element.createElement(wp.editor.RichText.Content, {
+      tagName: "p",
+      className: "unlock-protocol__locked",
+      value: props.attributes.content
+    })
+  }
+})
+
+// Block for content that is only visible while the status of the lock is pending (loading)
+// We just extend the coreButton functionality when it's loaded
+registerBlockType("unlock/checkout-button", {
+  title: "Checkout Button - Visible to non members only",
+  icon: "lock",
+  category: "common",
+  attributes: {
+    anchor: { type: "string" }
+  },
+
+  edit: props => {
+    const onChangeContent = value => {
+      props.setAttributes({ anchor: value })
+    }
+    const button = wp.element.createElement(wp.editor.RichText, {
+      placeholder: "Become a member now!",
+      value: props.attributes.anchor,
+      onChange: onChangeContent,
+      className: "wp-block-button__link"
+    })
+    return wp.element.createElement(
+      "div",
+      {
+        className: "wp-block-button"
+      },
+      [button]
+    )
+  },
+
+  save: props => {
+    const button = wp.element.createElement(wp.editor.RichText.Content, {
+      tagName: "a",
+      onClick:
+        "window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()",
+      value: props.attributes.anchor,
+      className: "wp-block-button__link"
+    })
+    return wp.element.createElement(
+      "div",
+      {
+        className: "wp-block-button unlock-protocol__locked"
+      },
+      [button]
+    )
+  }
+})
