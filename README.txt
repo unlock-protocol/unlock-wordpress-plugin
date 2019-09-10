70aa9909b309e54963a675739a7125ea50a7e2c1
Julien Genestoux <julien.genestoux@gmail.com>
2019-09-10 15:09:11 -0400

    first commit

---
+++ README.txt
@@ -0,0 +1,52 @@
+=== Unlock Protocol ===
+Contributors: julien51
+Donate link: https://donate.unlock-protocol.com/?r=unlock-protocol/unlock
+Tags: paywall, ethereum, monetization, unlock, membership, subscription, member
+Requires at least: 5.1
+Tested up to: 5.2
+Requires PHP: 7.2
+Stable tag: 1.0
+License: GPLv3
+License URI: https://github.com/unlock-protocol/unlock-wordpress-plugin/blob/master/LICENSE
+
+This plugin lets authors adds locks to theit posts and pages so that only paying visitors can view their content.
+
+== Description ==
+
+[Unlock](https://unlock-protocol.com/) is a protocol for memberships which lets any creator monetize their content in a permissionless way.
+
+You can [try a demo on this site](https://wordpress-demo.unlock-protocol.com/) where the content of the post changes on whether the visitor is a member or not.
+
+In order to become members, visitors need to be using a web3 enabled wallet such as [MetaMask](https://metamask.io/) or a web browser with an embeded wallet, such as [Opera](https://www.opera.com/crypto). They also need a balance of Ether in order to purchase keys to pay for the memberships. Ether can be bought on exchanges such as [Coinbase](https://www.coinbase.com/).
+
+== Getting Started ==
+
+If you'd like to add a lock to your site, start by [deploying your first lock](https://unlock-protocol.com/blog/create-first-lock/).
+
+While the lock is deploying you should install this plugin. Start by [downloading it](https://github.com/unlock-protocol/unlock-wordpress-plugin/archive/master.zip). Then, from the administration of your Wordpress.org website, in the `Plugins` section, click on the `Add New` button and then on the `Upload Plugin` button to upload the plugin.
+
+Once installed, check the plugin's settings to add the address of the lock that you have deployed.
+
+== Writing locked stories ==
+
+The plugin provides you with "Blocks" which can be used in the Gutember Editor, for both posts and pages. We provide 3 different blocks:
+
+- The "locked" block: a block whose content (rich text) is visible to non-members.
+- The "unlocked" block: a block whose content (rich text) is visible only to members.
+- The "checkout button" block: a block with a button to let you add a button for people to become members. This button will not be visible for members.
+
+Once your story includes the content you need, you can preview its content like any other WordPress post or page. We recommend the use of a web3 wallet which supports multiple accounts so that you can easily toggle between accounts which are already members and accounts which are not members yet!
+
+== Frequently Asked Questions ==
+
+To be completed with questions!
+
+== Changelog ==
+
+= 1.0 =
+Initial version
+
+== Upgrade Notice ==
+
+N/A
+
---
+++ unlock-protocol.php
@@ -0,0 +1,146 @@
+<?php
+/**
+* Plugin Name: Unlock Protocol Plugin
+* Plugin URI: https://github.com/unlock-protocol/unlock-wordpress-plugin
+* Description: A plugin to add lock(s) to blocks of content inside of Wordpress, on both pages and posts.
+* Version: 1.0
+* Author: Unlock
+* Author URI: https://unlock-protocol.com
+*/
+
+// Add the config to the head + styles
+function load_unlock() {
+  // TODO: add more settings (CTA, more locks... etc)
+  ?>
+  <script>
+  var unlockProtocolConfig = {
+    "locks": {
+      "<?php echo get_option('lock_address'); ?>":{}
+    },
+    "icon": "https://unlock-protocol.com/static/images/svg/unlock-word-mark.svg",
+    "callToAction": {
+      "default": "Become a member today!"
+    }
+  }
+  </script>
+  <style>
+  .unlock-protocol__unlocked, .unlock-protocol__locked {
+    display: none;
+  }
+  </style>
+  <?php
+  wp_enqueue_script( 'unlock_paywall_script', 'https://paywall.unlock-protocol.com/static/unlock.1.0.min.js');
+}
+add_action('wp_head', 'load_unlock');
+
+// Add the event listener at the end of the <body>
+function add_unlock_event_listener() {
+  ?>
+  <script>
+  window.addEventListener('unlockProtocol', function(e) {
+    var state = e.detail
+    document.querySelectorAll('.unlock-protocol__pending').forEach(element => {
+      element.style.display = "none";
+    })
+    if (state === 'locked') {
+      document.querySelectorAll('.unlock-protocol__unlocked').forEach(element => {
+        element.style.display = "none";
+      })
+      document.querySelectorAll('.unlock-protocol__locked').forEach(element => {
+        element.style.display = "block";
+      })
+    } else if (state === 'unlocked') {
+      document.querySelectorAll('.unlock-protocol__unlocked').forEach(element => {
+        element.style.display = "block";
+      })
+      document.querySelectorAll('.unlock-protocol__locked').forEach(element => {
+        element.style.display = "none";
+      })
+    }
+  })
+  </script>
+  <?php
+}
+add_action('wp_footer', 'add_unlock_event_listener');
+
+// Adds the blocks for the Gutember Editor
+function load_unlock_blocks() {
+  wp_enqueue_script(
+    'locked_block',
+    plugin_dir_url(__FILE__) . 'unlock-blocks.js',
+    array('wp-blocks','wp-editor'),
+    true
+  );
+}
+add_action('enqueue_block_editor_assets', 'load_unlock_blocks');
+
+
+// Adds Menu item for Unlock settings
+function add_unlock_menu_items() {
+  add_options_page("Unlock", "Unlock", "manage_options", "unlock", "unlock_options_page");
+}
+add_action("admin_menu", "add_unlock_menu_items");
+
+// Adds Unlock settings page
+function unlock_options_page() {
+  ?>
+  <h1>Unlock Settings</h1>
+  <form method="post" action="options.php">
+  <?php
+
+  //add_settings_section callback is displayed here. For every new section we need to call settings_fields.
+  settings_fields("header_section");
+
+  // all the add_settings_field callbacks is displayed here
+  do_settings_sections("unlock-settings");
+
+  // Add the submit button to serialize the options
+  submit_button();
+
+  ?>
+  </form>
+  <?php
+}
+
+// Displays the unlock options
+function display_unlock_options() {
+  //section name, display name, callback to print description of section, page to which section is attached.
+  add_settings_section("header_section", "", "display_header_options_content", "unlock-settings");
+
+  //setting name, display name, callback to print form element, page in which field is displayed, section to which it belongs.
+  //last field section is optional.
+  add_settings_field("lock_address", "Lock Address", "lock_address_form_element", "unlock-settings", "header_section");
+
+  //section name, form element name, callback for sanitization
+  register_setting("header_section", "lock_address");
+}
+add_action("admin_init", "display_unlock_options");
+
+// Header for the unlock options
+function display_header_options_content() {
+  ?>
+  <p>Once you have <a href="https://unlock-protocol.com/" target="_blank">deployed your lock</a>, please enter its address.</p>
+  <?php
+}
+
+// Section for the unlock options
+function lock_address_form_element() {
+  ?>
+  <input type="text" name="lock_address" id="lock_address" value="<?php echo get_option('lock_address'); ?>" />
+  <?php
+}
+
+
+add_filter('plugin_action_links_unlock-protocol/unlock-protocol.php', 'unlock_settings_link' );
+function unlock_settings_link($links) {
+  $url = esc_url( add_query_arg('page', 'unlock', get_admin_url() . 'admin.php') );
+  // Create the link.
+  $settings_link = "<a href='$url'>" . __( 'Settings' ) . '</a>';
+  // Adds the link to the end of the array.
+  array_push($links, $settings_link);
+  return $links;
+}
+
+
+
+?>
