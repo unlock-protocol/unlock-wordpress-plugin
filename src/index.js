8bbca7b90f1c344467fbad6831bf31f0912e05b3
Julien Genestoux <julien.genestoux@gmail.com>
2019-11-04 12:04:48 -0500

    moved to jsx

--- src/index.js
+++ src/index.js
@@ -1,111 +1,98 @@
-const { registerBlockType, getBlockTypes } = window.wp.blocks
-const { RichText } = wp.editor
+const { registerBlockType } = window.wp.blocks;
+const { RichText } = wp.editor;
 
 // Block for content that is only available to owners of keys!
-registerBlockType("unlock/unlocked-box", {
-  title: "Unlocked Box - Visible to members only",
-  icon: "lock",
-  category: "common",
-  attributes: {
-    content: { type: "string" }
-  },
+registerBlockType( 'unlock/unlocked-box', {
+	title: 'Unlocked Box - Visible to members only',
+	icon: 'lock',
+	category: 'common',
+	attributes: {
+		content: { type: 'string' },
+	},
 
-  edit: props => {
-    const onChangeContent = value => {
-      props.setAttributes({ content: value })
-    }
-    return wp.element.createElement(RichText, {
-      placeholder:
-        "Add your content which will only be visible by key holders!",
-      value: props.attributes.content,
-      onChange: onChangeContent,
-      className: "unlock__unlocked-box"
-    })
-  },
+	edit: ( { setAttributes, attributes } ) => {
+		const onChangeContent = ( value ) => {
+			setAttributes( { content: value } );
+		};
+		return <RichText
+			placeholder="Add your content which will only be visible by key holders!"
+			value={ attributes.content }
+			onChange={ onChangeContent }
+			className="unlock__unlocked-box"
+		/>;
+	},
 
-  save: props => {
-    return wp.element.createElement(RichText.Content, {
-      tagName: "p",
-      className: "unlock-protocol__unlocked",
-      value: props.attributes.content
-    })
-  }
-})
+	save: ( { attributes } ) => {
+		return <RichText.Content
+			tagName="p"
+			className="unlock-protocol__unlocked"
+			value={ attributes.content }
+		/>;
+	},
+} );
 
 // Block for content that is only available for people with no key
-registerBlockType("unlock/locked-box", {
-  title: "Locked Box - Visible to non members only",
-  icon: "lock",
-  category: "common",
-  attributes: {
-    content: { type: "string" }
-  },
+registerBlockType( 'unlock/locked-box', {
+	title: 'Locked Box - Visible to non members only',
+	icon: 'lock',
+	category: 'common',
+	attributes: {
+		content: { type: 'string' },
+	},
 
-  edit: props => {
-    const onChangeContent = value => {
-      props.setAttributes({ content: value })
-    }
-    return wp.element.createElement(RichText, {
-      placeholder:
-        "Add your content which will only be visible by users who do not have a key yet!",
-      value: props.attributes.content,
-      onChange: onChangeContent,
-      className: "lock__unlocked-box"
-    })
-  },
+	edit: ( { setAttributes, attributes } ) => {
+		const onChangeContent = ( value ) => {
+			setAttributes( { content: value } );
+		};
 
-  save: props => {
-    return wp.element.createElement(RichText.Content, {
-      tagName: "p",
-      className: "unlock-protocol__locked",
-      value: props.attributes.content
-    })
-  }
-})
+		return <RichText
+			placeholder="Add your content which will only be visible by users who do not have a key yet!"
+			value={ attributes.content }
+			onChange={ onChangeContent }
+			className="lock__unlocked-box" />;
+	},
+
+	save: ( { attributes } ) => {
+		return <RichText.Content
+			tagName="p"
+			className="unlock-protocol__locked"
+			value={ attributes.content }
+		/>;
+	},
+} );
 
 // Block for content that is only visible while the status of the lock is pending (loading)
 // We just extend the coreButton functionality when it's loaded
-registerBlockType("unlock/checkout-button", {
-  title: "Checkout Button - Visible to non members only",
-  icon: "lock",
-  category: "common",
-  attributes: {
-    anchor: { type: "string" }
-  },
+registerBlockType( 'unlock/checkout-button', {
+	title: 'Checkout Button - Visible to non members only',
+	icon: 'lock',
+	category: 'common',
+	attributes: {
+		anchor: { type: 'string' },
+	},
 
-  edit: props => {
-    const onChangeContent = value => {
-      props.setAttributes({ anchor: value })
-    }
-    const button = wp.element.createElement(RichText, {
-      placeholder: "Become a member now!",
-      value: props.attributes.anchor,
-      onChange: onChangeContent,
-      className: "wp-block-button__link"
-    })
-    return wp.element.createElement(
-      "div",
-      {
-        className: "wp-block-button"
-      },
-      [button]
-    )
-  },
+	edit: ( { setAttributes, attributes } ) => {
+		const onChangeContent = ( value ) => {
+			setAttributes( { anchor: value } );
+		};
 
-  save: props => {
-    const button = wp.element.createElement(RichText.Content, {
-      tagName: "a",
-      onClick:
-        "window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()",
-      value: props.attributes.anchor,
-      className: "wp-block-button__link"
-    })
-    return wp.element.createElement(
-      "div",
-      {
-        className: "wp-block-button unlock-protocol__locked"
-      },
-      [button]
-    )
-  }
-})
+		return <div className="wp-block-button">
+			<RichText
+				placeholder="Become a member now!"
+				value={ attributes.anchor }
+				onChange={ onChangeContent }
+				className="wp-block-button__link" />
+		</div>;
+	},
+
+	save: ( { attributes } ) => {
+		return <div className="wp-block-button unlock-protocol__locked">
+			<RichText.Content
+				tagName="a"
+				onClick="window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()"
+				value={ attributes.anchor }
+				className="wp-block-button__link"
+			/>
+		</div>;
+	},
+} );
