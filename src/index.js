ed6d2b9f63cdac08a16f93e2eeb7e559accea80c
Julien Genestoux <julien.genestoux@gmail.com>
2019-12-22 10:35:17 -0500

    removing the class from the button so it is more flexible

--- src/index.js
+++ src/index.js
@@ -1,83 +1,93 @@
-const { registerBlockType } = window.wp.blocks;
-const { RichText, InnerBlocks, InspectorControls } = wp.editor;
-const { PanelBody, SelectControl } = wp.components;
+const { registerBlockType } = window.wp.blocks
+const { RichText, InnerBlocks, InspectorControls } = wp.editor
+const { PanelBody, SelectControl } = wp.components
 
 // Block for content that is only available to owners of keys!
-registerBlockType( 'unlock/unlock-box', {
-	title: 'Unlock Protocol Block',
-	icon: 'lock',
-	category: 'common',
-	attributes: {
-		unlockState: {
-			type: 'string',
-			default: 'locked',
-		},
-	},
+registerBlockType("unlock/unlock-box", {
+  title: "Unlock Protocol Block",
+  icon: "lock",
+  category: "common",
+  attributes: {
+    unlockState: {
+      type: "string",
+      default: "locked"
+    }
+  },
 
-	edit: ( { attributes, setAttributes, className } ) => {
-		return [
-			<InspectorControls>
-				<PanelBody title={ 'Content Settings' }>
-					<SelectControl
-						label={ 'Hide this if user:' }
-						value={ attributes.unlockState }
-						options={
-							[
-								{ label: 'Is not a member', value: 'locked' },
-								{ label: 'Is a member', value: 'unlocked' },
-							]
-						}
-						onChange={ ( unlockState ) => {
-							setAttributes( { unlockState } );
-						} }
-					/>
-				</PanelBody>
-			</InspectorControls>,
-			<div className={ [ className, `unlock-protocol__${ attributes.unlockState }` ].join( ' ' ) }>
-				<InnerBlocks />
-			</div>,
-		];
-	},
+  edit: ({ attributes, setAttributes, className }) => {
+    return [
+      <InspectorControls>
+        <PanelBody title={"Content Settings"}>
+          <SelectControl
+            label={"Hide this if user:"}
+            value={attributes.unlockState}
+            options={[
+              { label: "Is not a member", value: "locked" },
+              { label: "Is a member", value: "unlocked" }
+            ]}
+            onChange={unlockState => {
+              setAttributes({ unlockState })
+            }}
+          />
+        </PanelBody>
+      </InspectorControls>,
+      <div
+        className={[
+          className,
+          `unlock-protocol__${attributes.unlockState}`
+        ].join(" ")}
+      >
+        <InnerBlocks />
+      </div>
+    ]
+  },
 
-	save: ( { attributes } ) => {
-		return <div className={ `unlock-protocol__${ attributes.unlockState }` }>
-			<InnerBlocks.Content />
-		</div>;
-	},
-} );
+  save: ({ attributes }) => {
+    return (
+      <div className={`unlock-protocol__${attributes.unlockState}`}>
+        <InnerBlocks.Content />
+      </div>
+    )
+  }
+})
 
 // Block for content that is only visible while the status of the lock is pending (loading)
 // We just extend the coreButton functionality when it's loaded
-registerBlockType( 'unlock/checkout-button', {
-	title: 'Checkout Button - Visible to non members only',
-	icon: 'lock',
-	category: 'common',
-	attributes: {
-		anchor: { type: 'string' },
-	},
+registerBlockType("unlock/checkout-button", {
+  title: "Checkout Button - Visible to non members only",
+  icon: "lock",
+  category: "common",
+  attributes: {
+    anchor: { type: "string" }
+  },
 
-	edit: ( { setAttributes, attributes } ) => {
-		const onChangeContent = ( value ) => {
-			setAttributes( { anchor: value } );
-		};
+  edit: ({ setAttributes, attributes }) => {
+    const onChangeContent = value => {
+      setAttributes({ anchor: value })
+    }
 
-		return <div className="wp-block-button">
-			<RichText
-				placeholder="Become a member now!"
-				value={ attributes.anchor }
-				onChange={ onChangeContent }
-				className="wp-block-button__link" />
-		</div>;
-	},
+    return (
+      <div className="wp-block-button">
+        <RichText
+          placeholder="Become a member now!"
+          value={attributes.anchor}
+          onChange={onChangeContent}
+          className="wp-block-button__link"
+        />
+      </div>
+    )
+  },
 
-	save: ( { attributes } ) => {
-		return <div className="wp-block-button unlock-protocol__locked">
-			<RichText.Content
-				tagName="a"
-				onClick="window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()"
-				value={ attributes.anchor }
-				className="wp-block-button__link"
-			/>
-		</div>;
-	},
-} );
+  save: ({ attributes }) => {
+    return (
+      <div className="wp-block-button">
+        <RichText.Content
+          tagName="a"
+          onClick="window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()"
+          value={attributes.anchor}
+          className="wp-block-button__link"
+        />
+      </div>
+    )
+  }
+})
