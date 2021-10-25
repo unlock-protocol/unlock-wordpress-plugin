db23f88095af6d0c17d606bb14d3ba53204f56b2
Kapil Paul <kapilpaul007@gmail.com>
2021-10-25 13:29:34 +0600

    fix: phpcs errors

--- unlock-protocol.php
+++ unlock-protocol.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Unlock Protocol Plugin
- * Description:  A plugin to add lock(s) to blocks of content inside of Wordpress, on both pages and posts. See https://www.ibenic.com/enable-inner-blocks-gutenberg/ for details about the implementation.
+ * Description:  A plugin to add lock(s) to blocks of content inside of WordPress, on both pages and posts. See https://www.ibenic.com/enable-inner-blocks-gutenberg/ for details about the implementation.
  * Plugin URI:  https://github.com/unlock-protocol/unlock-wordpress-plugin
  * Author:      Unlock
  * Author URI:  https://unlock-protocol.com
