44a68632aa9d7adeee12cc5e82b4878291f60af5
Kapil Paul <kapilpaul007@gmail.com>
2021-11-19 11:55:53 +0600

    fix: build files

--- assets/build/js/blocks.asset.php
+++ assets/build/js/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '02ea12cd0a703bfef590fbf4bbcaf1f6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '08c15c5e136070194e47f031ca9c870c');
