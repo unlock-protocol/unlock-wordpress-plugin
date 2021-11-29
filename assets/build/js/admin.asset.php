0b70c0b08a2137512858c0993b17a64a49b9d702
Kapil Paul <kapilpaul007@gmail.com>
2021-11-29 11:02:30 +0600

    fix: unlock doc link and default tab settings

--- assets/build/js/admin.asset.php
+++ assets/build/js/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => 'f1fec20f43874528cd3b1fa750ca3b81');
+<?php return array('dependencies' => array('jquery', 'react', 'wp-api-fetch', 'wp-components', 'wp-i18n'), 'version' => '9ad73ad01eeb32281b7546aff21fdb90');
