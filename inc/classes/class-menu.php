4cf680886f327ca3383118d742f7099270721595
Shardul Pathak <48183855+shapaaa@users.noreply.github.com>
2021-12-02 17:34:25 +0530

    Show admin notice if Anyone can register option is disabled

--- inc/classes/class-menu.php
+++ inc/classes/class-menu.php
@@ -26,10 +26,30 @@ class Menu {
 	 * @since 3.0.0
 	 */
 	protected function __construct() {
+		$register = true === (bool) get_option( 'users_can_register', false );
 
+		if ( ! $register ) {
+			add_action( 'admin_notices', array( $this, 'membership_admin_notice__error' ) );
+		}
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 		add_filter( 'plugin_action_links_' . UNLOCK_PROTOCOL_BASENAME_FILE, array( $this, 'plugin_action_links' ) );
 
+	}
+	/**
+	 * Show admin error notice if membership option is disabled
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return void
+	 */
+	public function membership_admin_notice__error() {
+		$class           = 'notice notice-error';
+		$message         = __( 'Unlock Protocol has detected that user registrations are disabled on this website. Please make sure that ', 'unlock-protocol' );
+		$anchor          = __( 'Settings > General > Anyone can register', 'unlock-protocol' );
+		$checked_message = __( 'is checked.', 'unlock-protocol' );
+		$url             = sprintf( '<a href="%1$s">%2$s<a/>', esc_url( admin_url( 'options-general.php' ) ), $anchor );
+
+		printf( '<div  class="%1$s"><p> %2$s %3$s %4$s </p></div>', esc_attr( $class ), esc_html( $message ), esc_html( $url ), esc_html( $checked_message ) );
 	}
 
 	/**
