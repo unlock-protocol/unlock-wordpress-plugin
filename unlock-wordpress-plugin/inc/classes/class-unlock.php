0f27fd4c8bd8a0b42ad2b35c46075050f8f1a591
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-17 16:41:13 -0400

    DRYing up code

--- unlock-wordpress-plugin/inc/classes/class-unlock.php
+++ unlock-wordpress-plugin/inc/classes/class-unlock.php
@@ -312,4 +312,110 @@ class Unlock {
 
 		return apply_filters( 'unlock_protocol_network_list', $networks );
 	}
+
+
+	/**
+	 * Render checkout button.
+	 *
+	 * @param array $locks locks.
+	 *
+	 * @return mixed|void
+	 */
+	public static function render_checkout_button( $locks ) {
+		$checkout_url = Unlock::get_checkout_url( $locks, get_permalink() );
+
+		$checkout_button_text       = up_get_general_settings( 'checkout_button_text', __( 'Purchase this', 'unlock-protocol' ) );
+		$checkout_button_bg_color   = up_get_general_settings( 'checkout_button_bg_color', '#000' );
+		$checkout_button_text_color = up_get_general_settings( 'checkout_button_text_color', '#fff' );
+		$blurred_image_activated    = wp_validate_boolean( up_get_general_settings( 'checkout_blurred_image_button', false ) );
+
+		$template_data = array(
+			'checkout_url'               => $checkout_url,
+			'checkout_button_text'       => $checkout_button_text,
+			'checkout_button_bg_color'   => $checkout_button_bg_color,
+			'checkout_button_text_color' => $checkout_button_text_color,
+			'blurred_image_activated'    => $blurred_image_activated,
+		);
+
+		// Fetching some more data if blurred image button type is activated.
+		if ( $blurred_image_activated ) {
+			$checkout_button_description = up_get_general_settings( 'checkout_button_description', __( 'To view this content please', 'unlock-protocol' ) );
+			$checkout_bg_image           = up_get_general_settings( 'checkout_bg_image' );
+
+			$template_data['checkout_button_description'] = $checkout_button_description;
+			$template_data['checkout_bg_image']           = $checkout_bg_image;
+		}
+
+		$html_template = unlock_protocol_get_template( 'login/checkout-button', $template_data );
+
+		return apply_filters( 'unlock_protocol_checkout_content', $html_template, $template_data );
+	}
+
+	/**
+	 * Render login button.
+	 *
+	 * @return mixed|void
+	 */
+	public static function render_login_button() {
+		$login_button_text       = up_get_general_settings( 'login_button_text', __( 'Login with Unlock', 'unlock-protocol' ) );
+		$login_button_bg_color   = up_get_general_settings( 'login_button_bg_color', '#000' );
+		$login_button_text_color = up_get_general_settings( 'login_button_text_color', '#fff' );
+		$blurred_image_activated = wp_validate_boolean( up_get_general_settings( 'login_blurred_image_button', false ) );
+
+		$template_data = array(
+			'login_url'               => Unlock::get_login_url( get_permalink() ),
+			'login_button_text'       => $login_button_text,
+			'login_button_bg_color'   => $login_button_bg_color,
+			'login_button_text_color' => $login_button_text_color,
+			'blurred_image_activated' => $blurred_image_activated,
+		);
+
+		// Fetching some more data if blurred image button type is activated.
+		if ( $blurred_image_activated ) {
+			$login_button_description = up_get_general_settings( 'login_button_description', __( 'To view this content please', 'unlock-protocol' ) );
+			$login_bg_image           = up_get_general_settings( 'login_bg_image' );
+
+			$template_data['login_button_description'] = $login_button_description;
+			$template_data['login_bg_image']           = $login_bg_image;
+		}
+
+		$html_template = unlock_protocol_get_template( 'login/button', $template_data );
+
+		return apply_filters( 'unlock_protocol_login_content', $html_template, $template_data );
+	}
+
+	/**
+	 * Render block.
+	 *
+	 * @param array  $locks List of attributes passed in block.
+	 * @param string $content post content.
+	 *
+	 * @since 4.0.0
+	 *
+	 * @return string HTML elements.
+	 */
+	public static function render_content( $locks, $content ) {
+		// Bail out if current user is admin or the author.
+		if ( current_user_can( 'manage_options' ) || ( get_the_author_meta( 'ID' ) === get_current_user_id() ) ) {
+			return $content;
+		}
+
+		if (
+			! is_user_logged_in() ||
+			( is_user_logged_in() && ! up_get_user_ethereum_address() )
+		) {
+			return Unlock::render_login_button();
+		}
+
+		$settings = get_option( 'unlock_protocol_settings', array() );
+		$networks = isset( $settings['networks'] ) ? $settings['networks'] : array();
+
+		if ( !Unlock::has_access( $networks, $locks ) ) {
+			return Unlock::render_checkout_button( $locks );
+		}
+		return $content;
+	}
+
+
+
 }
--- unlock-wordpress-plugin/inc/classes/fullpostpage/class-unlock-box-full-post-page.php
+++ unlock-wordpress-plugin/inc/classes/fullpostpage/class-unlock-box-full-post-page.php
@@ -59,113 +59,13 @@ class Unlock_Box_Full_Post_Page {
 		// Retrieve the saved attributes.
 		$attributes = get_post_meta( $post_id, 'unlock_protocol_post_locks', true );
 
-		// Check if the attributes are not empty.
 		if ( ! empty( $attributes ) ) {
 			$locks = json_decode($attributes, true);
-			// Call render_block() to trigger the Unlock Protocol flow.
-			return $this->render_block( $locks, $content );
-		}else{
-			
-			//it is essential to return content when lock(s) not set for a post/page
-			// because this is hooking to the full post content "the_content" wp hook
-			// if not, even post/pages without lock(s) applied will have there content hidden
-			//without a way to access or unlock there hidden content
+			// Call render_content() to trigger the Unlock Protocol flow.
+			return Unlock::render_content( $locks, $content );
+		} else {			
 			return $content;
 		}
 	}
-	
-	/**
-	 * Render block.
-	 *
-	 * @param array  $attributes List of attributes passed in block.
-	 * @param string $content Block Content.
-	 *
-	 * @since 4.0.0
-	 *
-	 * @return string HTML elements.
-	 */
-	public function render_block( $locks, $content ) {
-		// Bail out if current user is admin or the author.
-		if ( current_user_can( 'manage_options' ) || ( get_the_author_meta( 'ID' ) === get_current_user_id() ) ) {
-			return $content;
-		}
 
-		if (
-			! is_user_logged_in() ||
-			( is_user_logged_in() && ! up_get_user_ethereum_address() )
-		) {
-			$login_button_text       = up_get_general_settings( 'login_button_text', __( 'Login with Unlock', 'unlock-protocol' ) );
-			$login_button_bg_color   = up_get_general_settings( 'login_button_bg_color', '#000' );
-			$login_button_text_color = up_get_general_settings( 'login_button_text_color', '#fff' );
-			$blurred_image_activated = wp_validate_boolean( up_get_general_settings( 'login_blurred_image_button', false ) );
-
-			$template_data = array(
-				'login_url'               => Unlock::get_login_url( get_permalink() ),
-				'login_button_text'       => $login_button_text,
-				'login_button_bg_color'   => $login_button_bg_color,
-				'login_button_text_color' => $login_button_text_color,
-				'blurred_image_activated' => $blurred_image_activated,
-			);
-
-			// Fetching some more data if blurred image button type is activated.
-			if ( $blurred_image_activated ) {
-				$login_button_description = up_get_general_settings( 'login_button_description', __( 'To view this content please', 'unlock-protocol' ) );
-				$login_bg_image           = up_get_general_settings( 'login_bg_image' );
-
-				$template_data['login_button_description'] = $login_button_description;
-				$template_data['login_bg_image']           = $login_bg_image;
-			}
-
-			$html_template = unlock_protocol_get_template( 'login/button', $template_data );
-
-			return apply_filters( 'unlock_protocol_login_content', $html_template, $template_data );
-		}
-
-		$settings = get_option( 'unlock_protocol_settings', array() );
-		$networks = isset( $settings['networks'] ) ? $settings['networks'] : array();
-
-		if ( Unlock::has_access( $networks, $locks ) ) {
-			return $content;
-		}
-
-		return $this->get_checkout_url( $locks );
-	}
-
-	/**
-	 * Get checkout url for block
-	 *
-	 * @param array $locks locks.
-	 * @param array $networks networks from configuration.
-	 *
-	 * @return mixed|void
-	 */
-	private function get_checkout_url( $locks ) {
-		$checkout_url = Unlock::get_checkout_url( $locks, get_permalink() );
-
-		$checkout_button_text       = up_get_general_settings( 'checkout_button_text', __( 'Purchase this', 'unlock-protocol' ) );
-		$checkout_button_bg_color   = up_get_general_settings( 'checkout_button_bg_color', '#000' );
-		$checkout_button_text_color = up_get_general_settings( 'checkout_button_text_color', '#fff' );
-		$blurred_image_activated    = wp_validate_boolean( up_get_general_settings( 'checkout_blurred_image_button', false ) );
-
-		$template_data = array(
-			'checkout_url'               => $checkout_url,
-			'checkout_button_text'       => $checkout_button_text,
-			'checkout_button_bg_color'   => $checkout_button_bg_color,
-			'checkout_button_text_color' => $checkout_button_text_color,
-			'blurred_image_activated'    => $blurred_image_activated,
-		);
-
-		// Fetching some more data if blurred image button type is activated.
-		if ( $blurred_image_activated ) {
-			$checkout_button_description = up_get_general_settings( 'checkout_button_description', __( 'To view this content please', 'unlock-protocol' ) );
-			$checkout_bg_image           = up_get_general_settings( 'checkout_bg_image' );
-
-			$template_data['checkout_button_description'] = $checkout_button_description;
-			$template_data['checkout_bg_image']           = $checkout_bg_image;
-		}
-
-		$html_template = unlock_protocol_get_template( 'login/checkout-button', $template_data );
-
-		return apply_filters( 'unlock_protocol_checkout_content', $html_template, $template_data );
-	}
 }
