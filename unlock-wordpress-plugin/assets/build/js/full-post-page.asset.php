58cd2921257559f0e78c16eb12dcf87e851380b2
Julien Genestoux <julien.genestoux@gmail.com>
2023-04-16 22:39:37 -0400

    removed build files

--- unlock-wordpress-plugin/assets/build/js/full-post-page.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '1df0b09507f3e6f1b230');
